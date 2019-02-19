af5d11f1a043ab63985c6bd7cc64ea92aec86f27
Joshua Driesman <jdriesman@outlook.com>
2019-02-18 21:08:31 -0500

    Cleaned up tests

--- MatchingParens/tests.py
+++ MatchingParens/tests.py
@@ -4,13 +4,15 @@ from solution import is_matching_parens
 
 
 class TestMatchingParens(unittest.TestCase):
+    def setUp(self):
+        self.trueTestCases = ["", "()", "[()]", "[([])]"]
+        self.falseTestCases = ["[(])", "[(]"]
+
     def test(self):
-        self.assertTrue(is_matching_parens(""))
-        self.assertTrue(is_matching_parens("()"))
-        self.assertTrue(is_matching_parens("[()]"))
-        self.assertTrue(is_matching_parens("[([])]"))
-        self.assertFalse(is_matching_parens("[(])"))
-        self.assertFalse(is_matching_parens("[(]"))
+        for case in self.trueTestCases:
+            self.assertTrue(is_matching_parens(case))
+        for case in self.falseTestCases:
+            self.assertFalse(is_matching_parens(case))
 
 
 if __name__ == '__main__':
