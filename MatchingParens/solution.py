32c30e75f6f1f7124abbe20ea0f3e3802b1febc3
Joshua Driesman <jdriesman@outlook.com>
2019-02-18 20:08:02 -0500

    cleaner test print out

--- MatchingParens/solution.py
+++ MatchingParens/solution.py
@@ -19,9 +19,17 @@ def is_matching_parens(input):
     return q.empty()
 
 
-print("Should be True, was %s" % is_matching_parens(""))
-print("Should be True, was %s" % is_matching_parens("()"))
-print("Should be True, was %s" % is_matching_parens("[()]"))
-print("Should be True, was %s" % is_matching_parens("[([])]"))
-print("Should be False, was %s" % is_matching_parens("[(])"))
-print("Should be False, was %s" % is_matching_parens("[(]"))
+def print_test_result(inp, expected, output):
+    """
+    Prints out a preformatted string with the input, output, and expected value for a solution test.
+    """
+    print("The input was %s, the expected output was %s, and the actual output was %s." %
+          (inp, output, expected))
+
+
+print_test_result("", True, is_matching_parens(""))
+print_test_result("()", True, is_matching_parens("()"))
+print_test_result("[()]", True, is_matching_parens("[()]"))
+print_test_result("[([])]", True, is_matching_parens("[([])]"))
+print_test_result("[(])", False, is_matching_parens("[(])"))
+print_test_result("[(]", False, is_matching_parens("[(]"))
